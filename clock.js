c8f13d6cce61768b8efb874cc67e9e0aba14d679
Brandon <69767056+OtakuxCode@users.noreply.github.com>
2021-06-20 12:07:07 -0400

    Cleaned up

--- clock.js
+++ clock.js
@@ -1,93 +1,76 @@
 'use strict';
 
-function showTimeAndDate() {
-  
-  let dateObj = new Date();
-  let y = '0';
+function showTime() {
+  const theTime = new Date();
 
-  // Hours
-let hour = dateObj.getHours();
-if (hour < 10) {
-  hour =  y + hour;
- }
- if (hour > 12) {
-   hour = hour - 12;
- }
- 
-
-// Minutes
-let minutes = dateObj.getMinutes();
-if (minutes < 10) {
-  minutes =  y + minutes;
- }
-
- // Seconds
-let seconds = dateObj.getSeconds();
-if (seconds < 10) {
-  seconds =  y + seconds;
- }
+let hour = addLeadingZero(theTime.getHours());
+const minutes = addLeadingZero(theTime.getMinutes());
+const seconds = addLeadingZero(theTime.getSeconds());
+const isAm = hour < 12 || hour === 0;
+let amPm = isAm ? 'AM' : 'PM';
 
  // Current Time
-const currentTime = `${hour}:${minutes}:${seconds}`;
-
-// AM PM Calculator
-let ampm = dateObj.getHours();
-
-if (ampm < 12) {
-  ampm = 'AM';
-} else {
-  ampm = 'PM'
-};
+const currentTime = `${formatHour(hour)}:${minutes}:${seconds} ${amPm}`;
 
 
-// Day
-// let weekDay = dateObj.toLocaleString("Default", { weekday: "long"});
-let weekDay = dateObj.getDay();
+let timeOne = document.querySelector('.timeOne');
+timeOne.textContent = currentTime;
+}
+
+function addLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+
+function formatHour(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
+
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
+}
+
+
+
+function showDate() {
+  
+const theDate = new Date();
+const weekDay = theDate.getDay();
+const day = weekDays[weekDay];
+const dayOfMonth = theDate.getMonth();
+const month = months[dayOfMonth];
+const date = theDate.getDate();
+const currentYear = theDate.getFullYear();
+const timeTwo = document.querySelector('.timeTwo')
+
+
+timeTwo.textContent = `${day}, ${month} ${formatDateSuffix(date)} ${currentYear}`
+}
+
+function formatDateSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
+  }
+  return `${date}th`;
+}
+
 
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let day = weekDays[weekDay];
-
-
-// Month
-// let month = dateObj.toLocaleString("Default", {month: "long"});
-let dayOfMonth = dateObj.getMonth();
-
 const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-let month = months[dayOfMonth];
+showTime();
+showDate();
 
-// Numerical Day
-let numericalDate = dateObj.getDate();
-
-
-
-if(numericalDate == 1 || numericalDate == 21 || numericalDate == 31) {
-   numericalDate = numericalDate + "st";
-} else if (numericalDate == 2 || numericalDate == 22 ) {
-   numericalDate = numericalDate + "nd";
-}  else if (numericalDate == 3 || numericalDate == 23) {
-  numericalDate = numericalDate + "rd";
-} else  {
-  numericalDate = numericalDate + "th";
-}
-
-
-// Year
-let currentYear = dateObj.getFullYear();
-
-
-
-// Adding to page
-let timeOne = document.querySelector('.timeOne');
-let timeTwo = document.querySelector('.timeTwo')
-
-timeOne.textContent = `${hour}:${minutes}:${seconds} ${ampm}`;
-
-timeTwo.textContent = `${day}, ${month} ${numericalDate} ${currentYear}`
-
-}
-
-setInterval(showTimeAndDate, 1000);
+setInterval(() => {
+  showTime();
+  showDate();
+}, 1000);
 
 
 
